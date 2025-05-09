cb877f9cad7cf9517169b19de5b14cba39fea6a0
luanvsalmeida <luanalmeida@alunos.utfpr.edu.br>
2025-05-09 11:40:46 -0300

    fix: DB initialization
    - wait for it script
    - app waits for db (fixed)

--- src/api/app/api/db/session.py
+++ src/api/app/api/db/session.py
@@ -5,13 +5,21 @@ from .config import DATABASE_URL
 if DATABASE_URL == "":
     raise NotImplementedError("DATABASE_URL needs to be set")
 
-engine = sqlmodel.create_engine(DATABASE_URL)
+_engine = None
 
+def get_engine():
+    global _engine
+    if _engine is None:
+        _engine = sqlmodel.create_engine(DATABASE_URL)
+    return _engine
+
+
+
+def get_session():
+    engine = get_engine()
+    with Session(engine) as session:
+        yield session
 
 def init_db():
     print("creating database")
-    SQLModel.metadata.create_all(engine)
-
-def get_session():
-    with Session(engine) as session:
-        yield session 
+    SQLModel.metadata.create_all(get_engine())
